c63aa8f2a5ac19341152e7199425594d9ef345ee
Laura Wu <85155607+laurawulinjie@users.noreply.github.com>
2025-05-20 14:42:44 +0100

    fix-set useQuizStore persist to true

--- stores/useQuizStore.ts
+++ stores/useQuizStore.ts
@@ -9,50 +9,54 @@ function shuffleAnswers(q: Question): Question {
   };
 }
 
-export const useQuizStore = defineStore("quiz", () => {
-  const questions = ref<Question[]>(
-    questionsJson.map((q) =>
-      shuffleAnswers({ ...q, type: q.type as QuestionType })
-    )
-  );
-
-  const currentIndex = ref(0);
-  const score = ref(0);
-  const answersGiven = ref<boolean[]>([]);
-  const currentQuestion = computed(() => questions.value[currentIndex.value]);
-
-  const isFinished = computed(
-    () => currentIndex.value >= questions.value.length - 1
-  );
-
-  function answerQuestion(isCorrect: boolean) {
-    answersGiven.value[currentIndex.value] = isCorrect;
-    if (isCorrect) score.value++;
-  }
-
-  function nextQuestion() {
-    if (!isFinished.value) currentIndex.value++;
-  }
-
-  function resetQuiz() {
-    questions.value = questionsJson.map((q) =>
-      shuffleAnswers({ ...q, type: q.type as QuestionType })
+export const useQuizStore = defineStore(
+  "quiz",
+  () => {
+    const questions = ref<Question[]>(
+      questionsJson.map((q) =>
+        shuffleAnswers({ ...q, type: q.type as QuestionType })
+      )
     );
 
-    currentIndex.value = 0;
-    score.value = 0;
-    answersGiven.value = [];
-  }
+    const currentIndex = ref(0);
+    const score = ref(0);
+    const answersGiven = ref<boolean[]>([]);
+    const currentQuestion = computed(() => questions.value[currentIndex.value]);
 
-  return {
-    questions,
-    currentQuestion,
-    currentIndex,
-    score,
-    answersGiven,
-    isFinished,
-    answerQuestion,
-    nextQuestion,
-    resetQuiz,
-  };
-});
+    const isFinished = computed(
+      () => currentIndex.value >= questions.value.length - 1
+    );
+
+    function answerQuestion(isCorrect: boolean) {
+      answersGiven.value[currentIndex.value] = isCorrect;
+      if (isCorrect) score.value++;
+    }
+
+    function nextQuestion() {
+      if (!isFinished.value) currentIndex.value++;
+    }
+
+    function resetQuiz() {
+      questions.value = questionsJson.map((q) =>
+        shuffleAnswers({ ...q, type: q.type as QuestionType })
+      );
+
+      currentIndex.value = 0;
+      score.value = 0;
+      answersGiven.value = [];
+    }
+
+    return {
+      questions,
+      currentQuestion,
+      currentIndex,
+      score,
+      answersGiven,
+      isFinished,
+      answerQuestion,
+      nextQuestion,
+      resetQuiz,
+    };
+  },
+  { persist: true }
+);
